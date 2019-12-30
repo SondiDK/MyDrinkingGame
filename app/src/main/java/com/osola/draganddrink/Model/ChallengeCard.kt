1c4556dbb67791896bd3bbf7e4313876c430ead3
Oliver <olle_s_larsen@hotmail.com>
2019-12-31 00:27:39 +0100

    more challenges

--- app/src/main/java/com/osola/draganddrink/Model/ChallengeCard.kt
+++ app/src/main/java/com/osola/draganddrink/Model/ChallengeCard.kt
@@ -49,7 +49,25 @@ class ChallengeCard: Card() {
                 "One by one, make up a title for each players’ movie about their life ",
                 "Lick mayonnaise off of someone's toe.",
                 "Pick up a random book and read it in the most seductive voice you can manage ",
-                "Take a naked selfie and send it to your partner."
+                "Take a naked selfie and send it to your partner.",
+                "Write a poem about the given situation",
+                "Spin around 10 times and try to walk straight.",
+                "Call a 7-Eleven and ask if they’re open",
+                "Go outside and try to summon the rain.",
+                "Be blindfolded for the rest of the game.",
+                "Twerk to the current song.",
+                "Give everyone in the room a hug.",
+                "Hold hands with the person next to you.",
+                "Let each player choose one word, then attempt to form a sentence with it and post it to Facebook.",
+                "Let each person in the group slap you as hard as they can on your butt.",
+                "Have the person to your right do 10 squats while you lie underneath them.",
+                "Put an ice cube down the person to your rights shirt.",
+                "Get a person to sign your chest.",
+                "High-five the most strangers you can in 60 seconds.",
+                "Close your eyes and send a blind text to a random person",
+                "Give the person to your right a hickie",
+                "From now on, end every sentence with gugi"
+
             )
         }
 
