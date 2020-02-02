ff72d5f614e8c2bbee8cc832321e2db42cbb265f
Oliver <olle_s_larsen@hotmail.com>
2020-02-02 19:59:41 +0100

    minor test

--- app/src/main/java/com/osola/draganddrink/Fragments/PlayerFragment.kt
+++ app/src/main/java/com/osola/draganddrink/Fragments/PlayerFragment.kt
@@ -22,7 +22,7 @@ class PlayerFragment : Fragment(), PlayerManager.ValueChangeListener {
     private lateinit var currentPlayerNameView: TextView
     private lateinit var roundView: TextView
     private var activityCallback: listenerGameOver?  = null
-    private var totalNumberOfRoundsPlayed = 10
+    private var totalNumberOfRoundsPlayed = 11
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val view =  inflater.inflate(R.layout.player_fragment, container, false)
