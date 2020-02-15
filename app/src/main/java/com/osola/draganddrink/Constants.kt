279556510e62e298e45ea3ed9ed78cf1abaff690
Oliver <olle_s_larsen@hotmail.com>
2020-02-15 17:24:05 +0100

    game over dialog UI

--- app/src/main/java/com/osola/draganddrink/Constants.kt
+++ app/src/main/java/com/osola/draganddrink/Constants.kt
@@ -1,4 +1,5 @@
 package com.osola.draganddrink
 
 const val PLAYER_NAMES_KEY = "PLAYER_NAMES"
-const val PRELOAD_PLAYERS = false
+const val PRELOAD_PLAYERS = true
+
--- app/src/main/java/com/osola/draganddrink/MainActivity.kt
+++ app/src/main/java/com/osola/draganddrink/MainActivity.kt
@@ -54,6 +54,7 @@ class MainActivity : AppCompatActivity(), View.OnDragListener, View.OnTouchListe
         playerFragment.setPlayernames(names);
 
 
+
     }
 
 
